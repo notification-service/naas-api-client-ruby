400a2320ac61400f176e83019c81f0c49897b491
Nate Klaiber <nate@theklaibers.com>
2018-10-18 11:35:22 -0400

    Create a subscriber email address

--- lib/naas/requests/subscriber_email_addresses.rb
+++ lib/naas/requests/subscriber_email_addresses.rb
@@ -37,6 +37,29 @@ module Naas
 
         Naas::Response.new(request)
       end
+
+      # Created a new record
+      #
+      # @param params [Hash]
+      #
+      # @return [Naas::Response]
+      def self.create(params={})
+        rel   = Naas::Client.rel_for('rels/subscriber-email-addresses')
+        route = Naas::Client.routes.find_by_rel(rel)
+        url   = route.url_for
+
+        request_body = {
+          :subscriber_email_address => params
+        }
+
+        request = Naas::Client.connection.post do |req|
+          req.url(url)
+          req.headers['Accept'] = 'application/vnd.naas.json; version=1'
+          req.body = MultiJson.dump(request_body)
+        end
+
+        Naas::Response.new(request)
+      end
     end
   end
 end
