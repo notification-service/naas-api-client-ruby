672bf40965ef600393a1c2a9bc5fe546c58cecab
Nate Klaiber <nate@theklaibers.com>
2018-12-19 17:46:22 -0500

    Ability to create a subscriber

--- lib/naas/models/subscribers.rb
+++ lib/naas/models/subscribers.rb
@@ -57,6 +57,36 @@ module Naas
         Naas::Models::Subscriber.new(klass_attributes)
       end
 
+      # Create a new subscriber
+      #
+      # @raises [Naas::InvalidRequestError]
+      #
+      # @return [Naas::Models::Subscriber]
+      def self.create(params={})
+        request = Naas::Requests::Subscribers.create(params)
+
+        request.on(:success) do |resp|
+          response_body = resp.body
+          response_data = response_body.fetch('data', {})
+
+          klass_attributes = response_data
+
+          return Naas::Models::Subscriber.new(klass_attributes)
+        end
+
+        request.on(:failure) do |resp|
+          response_body = resp.body
+          response_data = response_body.fetch('data', {})
+
+          error           = Naas::Models::Error.new(response_data)
+          failure_message = "Failure creating the record: %s" % [error.full_messages.inspect]
+
+          Naas::Client.configuration.logger.info { failure_message }
+
+          raise Naas::Errors::InvalidRequestError.new(failure_message)
+        end
+      end
+
       def each(&block)
         internal_collection.each(&block)
       end
