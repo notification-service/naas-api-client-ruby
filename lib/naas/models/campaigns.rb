807d7f131a261de5808b05585839cbcc205bc6de
Nate Klaiber <nate@theklaibers.com>
2019-01-08 13:34:38 -0500

    Managing the campaigns through the nested scope
    This is to keep the requests explicit and requiring
    the entire tree.

--- lib/naas/models/campaigns.rb
+++ lib/naas/models/campaigns.rb
@@ -87,6 +87,81 @@ module Naas
         end
       end
 
+      # Helper method to retrieve from the
+      # request
+      #
+      # @return [Naas::Models::Campaigns]
+      def self.list_by_project_id(project_id, params={})
+        request = Naas::Requests::Campaigns.list_by_project_id(project_id, params)
+
+        klass_attributes = []
+
+        request.on(:success) do |resp|
+          response_body = resp.body
+          response_data = response_body.fetch('data', [])
+
+          klass_attributes = response_data
+        end
+
+        request.on(:failure) do |resp|
+          Naas::Client.configuration.logger.info { ("Failure retrieving the campaigns: %s" % [resp.status]) }
+        end
+
+        self.new(klass_attributes)
+      end
+
+      # Helper method to retrieve from the request
+      #
+      # @return [Naas::Models::Campaign]
+      def self.retrieve_by_project_id(project_id, id, params={})
+        request = Naas::Requests::Campaigns.retrieve_by_project_id(project_id, id, params)
+
+        klass_attributes = {}
+
+        request.on(:success) do |resp|
+          response_body = resp.body
+          response_data = response_body.fetch('data', {})
+
+          klass_attributes = response_data
+        end
+
+        request.on(:failure) do |resp|
+          Naas::Client.configuration.logger.info { ("Failure retrieving the campaign: %s" % [resp.status]) }
+        end
+
+        Naas::Models::Campaign.new(klass_attributes)
+      end
+
+      # Create a new campaign
+      #
+      # @raises [Naas::InvalidRequestError]
+      #
+      # @return [Naas::Models::Campaign]
+      def self.create_by_project_id(project_id, params={})
+        request = Naas::Requests::Campaigns.create_by_project_id(project_id, params)
+
+        request.on(:success) do |resp|
+          response_body = resp.body
+          response_data = response_body.fetch('data', {})
+
+          klass_attributes = response_data
+
+          return Naas::Models::Campaign.new(klass_attributes)
+        end
+
+        request.on(:failure) do |resp|
+          response_body = resp.body
+          response_data = response_body.fetch('data', {})
+
+          error           = Naas::Models::Error.new(response_data)
+          failure_message = "Failure creating the record: %s" % [error.full_messages.inspect]
+
+          Naas::Client.configuration.logger.info { failure_message }
+
+          raise Naas::Errors::InvalidRequestError.new(failure_message)
+        end
+      end
+
       def each(&block)
         internal_collection.each(&block)
       end
--- lib/naas/requests/campaigns.rb
+++ lib/naas/requests/campaigns.rb
@@ -19,6 +19,69 @@ module Naas
         Naas::Response.new(request)
       end
 
+      # Retrieve the list of campaigns by project
+      #
+      # @param project_id [String]
+      # @param params [Hash]
+      #
+      # @return [Naas::Response]
+      def self.list_by_project_id(project_id, params={})
+        rel   = Naas::Client.rel_for('rels/project-campaigns')
+        route = Naas::Client.routes.find_by_rel(rel)
+        url   = route.url_for(params.merge!(project_id: project_id))
+
+        request = Naas::Client.connection.get do |req|
+          req.url(url)
+          req.headers['Accept'] = 'application/vnd.naas.json; version=1'
+        end
+
+        Naas::Response.new(request)
+      end
+
+      # Retrieve the instance of a campaign by project
+      #
+      # @param project_id [String]
+      # @param id [String]
+      # @param params [Hash]
+      #
+      # @return [Naas::Response]
+      def self.retrieve_by_project_id(project_id, id, params={})
+        rel   = Naas::Client.rel_for('rels/project-campaign')
+        route = Naas::Client.routes.find_by_rel(rel)
+        url   = route.url_for(params.merge!(project_id: project_id, id: id))
+
+        request = Naas::Client.connection.get do |req|
+          req.url(url)
+          req.headers['Accept'] = 'application/vnd.naas.json; version=1'
+        end
+
+        Naas::Response.new(request)
+      end
+
+      # Create a new record
+      #
+      # @param id [Integer]
+      # @param params [Hash]
+      #
+      # @return [Naas::Response]
+      def self.create_by_project_id(project_id, params={})
+        rel   = Naas::Client.rel_for('rels/project-campaigns')
+        route = Naas::Client.routes.find_by_rel(rel)
+        url   = route.url_for(params.merge!(project_id: project_id))
+
+        request_body = {
+          :campaign => params
+        }
+
+        request = Naas::Client.connection.post do |req|
+          req.url(url)
+          req.headers['Accept'] = 'application/vnd.naas.json; version=1'
+          req.body = MultiJson.dump(request_body)
+        end
+
+        Naas::Response.new(request)
+      end
+
       # Retrieve the instance of a campaign
       #
       # @param id [Integer]
