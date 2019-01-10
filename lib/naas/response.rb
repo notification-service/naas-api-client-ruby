3467b0ce72b8ff7467b0137b466ba4051c8de1e2
Nate Klaiber <nate@theklaibers.com>
2019-01-10 12:04:09 -0500

    Adding ability for the client to return enriched models in the `Response`
    This is helpful to normalize links, pagination, and
    data

--- lib/naas/response.rb
+++ lib/naas/response.rb
@@ -51,6 +51,96 @@ module Naas
       self.headers.fetch('allow', '')
     end
 
+    def json?
+      self.body.is_a?(Hash)
+    end
+
+    def data_attributes
+      if self.json?
+        self.body['data']
+      end
+    end
+
+    def data_attributes?
+      !self.data.nil?
+    end
+
+    def pagination_body_attributes
+      if self.json?
+        self.body['pagination']
+      end
+    end
+
+    def pagination_body_attributes?
+      !self.pagination_body_attributes.nil?
+    end
+
+    def pagination_header_attributes
+      {
+        'page'             => self.headers['x-pagination-page'].to_i,
+        'per_page'         => self.headers['x-pagination-perpage'].to_i,
+        'total'            => self.headers['x-pagination-total'].to_i,
+        'maximum_per_page' => self.headers['x-pagination-maximumperpage'].to_i
+      }
+    end
+
+    def pagination_header_attributes?
+      self.headers.has_key?('x-pagination-page') &&
+      self.headers.has_key?('x-pagination-perpage') &&
+      self.headers.has_key?('x-pagination-total') &&
+      self.headers.has_key?('x-pagination-maximumperpage')
+    end
+
+    def resolved_pagination_attributes
+      resolved_attributes = {}
+
+      if self.pagination_body_attributes?
+        resolved_attributes = self.pagination_body_attributes
+      elsif self.pagination_header_attributes?
+        resolved_attributes = self.pagination_header_attributes
+      else
+        {}
+      end
+    end
+
+    def resolved_pagination_attributes?
+      !self.resolved_pagination_attributes.empty?
+    end
+
+    def pagination
+      if self.resolved_pagination_attributes?
+        Naas::Models::Pagination.new(self.resolved_pagination_attributes)
+      end
+    end
+
+    def pagination?
+      !self.pagination.nil?
+    end
+
+    def links_header
+      self.headers.fetch('link', '')
+    end
+
+    def links_header?
+      self.headers.has_key?('link')
+    end
+
+    def links_attributes
+      if self.json?
+        self.body['links']
+      else
+        []
+      end
+    end
+
+    def links_attributes?
+      self.links_attributes.any?
+    end
+
+    def links?
+      self.links.any?
+    end
+
     def allowed_methods
       self.allow_header.split(',').map(&:strip).map(&:upcase)
     end
