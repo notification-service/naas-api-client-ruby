74fe6c563c1552b20ea909d6a6480a2dbae74956
Nate Klaiber <nate@theklaibers.com>
2019-08-15 16:41:26 -0400

    Ability to create or update the project subscriber property

--- lib/naas/models/project_subscriber_properties.rb
+++ lib/naas/models/project_subscriber_properties.rb
@@ -55,6 +55,55 @@ module Naas
         end
       end
 
+      # Create a new record
+      #
+      # @param project_id [String]
+      # @param project_subscriber_id [String]
+      # @param params [Hash]
+      #
+      # @return [Naas::Models::ProjectSubscriberProperty]
+      def self.create_by_project_id_and_project_subscriber_id(project_id, project_subscriber_id, params={})
+        request = Naas::Requests::ProjectSubscriberProperties.create_by_project_id_and_project_subscriber_id(project_id, project_subscriber_id, params)
+
+        request.on(:success) do |resp|
+          return Naas::Models::ProjectSubscriberProperty.new(resp.data_attributes)
+        end
+
+        request.on(:failure) do |resp|
+          error           = Naas::Models::Error.new(resp.data_attributes)
+          failure_message = "Failure creating the record: %s" % [error.full_messages.inspect]
+
+          Naas::Client.configuration.logger.error { failure_message }
+
+          raise Naas::Errors::InvalidRequestError.new(failure_message)
+        end
+      end
+
+      # Update an existing record
+      #
+      # @param project_id [String]
+      # @param project_subscriber_id [String]
+      # @param id [String]
+      # @param params [Hash]
+      #
+      # @return [Naas::Models::ProjectSubscriberProperty]
+      def self.update_by_project_id_and_project_subscriber_id(project_id, project_subscriber_id, id, params={})
+        request = Naas::Requests::ProjectSubscriberProperties.update_by_project_id_and_project_subscriber_id(project_id, project_subscriber_id, id, params)
+
+        request.on(:success) do |resp|
+          return Naas::Models::ProjectSubscriberProperty.new(resp.data_attributes)
+        end
+
+        request.on(:failure) do |resp|
+          error           = Naas::Models::Error.new(resp.data_attributes)
+          failure_message = "Failure creating the record: %s" % [error.full_messages.inspect]
+
+          Naas::Client.configuration.logger.error { failure_message }
+
+          raise Naas::Errors::InvalidRequestError.new(failure_message)
+        end
+      end
+
       def each(&block)
         internal_collection.each(&block)
       end
