4dc866dfb0bb5b778869548277c10cd0de7f95b7
Nate Klaiber <nate@theklaibers.com>
2019-08-15 13:24:23 -0400

    Retrieve a list of project subscriber properties

--- lib/naas/models/project_subscriber_properties.rb
+++ lib/naas/models/project_subscriber_properties.rb
@@ -12,6 +12,27 @@ module Naas
         @collection = Array(collection)
       end
 
+      # Retrieve the list of subscribers properties by project
+      #
+      # @param project_id [String]
+      # @param project_subscriber_id [String]
+      # @param params [Hash]
+      #
+      # @return [Naas::Models::ProjectSubscriberProperties]
+      def self.list_by_project_id_and_project_subscriber_id(project_id, project_subscriber_id, params={})
+        request = Naas::Requests::ProjectSubscriberProperties.list_by_project_id_and_project_subscriber_id(project_id, project_subscriber_id, params)
+
+        request.on(:success) do |resp|
+          return Naas::Models::ProjectSubscriberProperties.new(resp.data_attributes)
+        end
+
+        request.on(:failure) do |resp|
+          Naas::Client.configuration.logger.error { ("Failure retrieving the campaigns: %s" % [resp.status]) }
+
+          nil
+        end
+      end
+
       def each(&block)
         internal_collection.each(&block)
       end
