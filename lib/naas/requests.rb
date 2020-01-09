6ff0f56f9439675b827f72377b233453f287b84c
Nate Klaiber <nate@theklaibers.com>
2020-01-09 13:02:14 -0500

    Add Requests model for Addons

--- lib/naas/requests.rb
+++ lib/naas/requests.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../requests/directory', __FILE__)
 require File.expand_path('../requests/data_types', __FILE__)
+require File.expand_path('../requests/addons', __FILE__)
 require File.expand_path('../requests/projects', __FILE__)
 require File.expand_path('../requests/project_properties', __FILE__)
 require File.expand_path('../requests/project_subscribers', __FILE__)
