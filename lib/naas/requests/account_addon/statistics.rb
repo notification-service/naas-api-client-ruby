80c61386ceb865633f7efc802e6e33cea9f3b457
Nate Klaiber <nate@theklaibers.com>
2020-04-14 12:03:05 -0400

    Request to retrieve network performers

--- lib/naas/requests/account_addon/statistics.rb
+++ lib/naas/requests/account_addon/statistics.rb
@@ -52,6 +52,31 @@ module Naas
 
           Naas::Response.new(request)
         end
+
+        # Retrieve the network top performers
+        #
+        # @param id [String]
+        # @param params [Hash]
+        #
+        # @return [Naas::Response]
+        def self.retrieve_network_performers(id, params={}, options={})
+          default_options = {
+            :accept => 'application/vnd.naas.json; version=1'
+          }
+
+          core_options = default_options.merge!(options)
+
+          rel   = Naas::Client.rel_for('rels/account-addon-statistics-network-performers')
+          route = Naas::Client.routes.find_by_rel(rel)
+          url   = route.url_for(params.merge!(account_addon_id: id))
+
+          request = Naas::Client.connection.get do |req|
+            req.url(url)
+            req.headers['Accept'] = core_options[:accept]
+          end
+
+          Naas::Response.new(request)
+        end
       end
     end
   end
